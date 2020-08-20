a3ec3759c0415ae9adbdbddc3dca677019ac90cd
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 19:51:00 +0800

    Добавьте файлы проекта.

---
+++ Sanch.Unsafe_Code.Ukazately/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.Unsafe_Code.Ukazately
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //указатели для передачи ссылки памяти
+            //надо в свойствах проекта разрешить выполняться небезопасному коду
+            unsafe //область небезопастного кода (для значимых переменных ). Ссылочные и так можно
+            {
+                int i = 30;
+                int* address = &i; //указатель адресса переменной i в памяти
+                Console.WriteLine(*address); //вывод указателя(получили значение)
+                Console.WriteLine((long)address); //получили адресс той ячейки памяти (его номер)(адресс первой ячейки, но она может занимать еще ячейки)
+
+                int a = 5;
+                int b = 7;
+
+                
+
+                Calc(a, &b);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+
+                //переход от одного участка памяти к другому
+                int* address2 = address + 4;//интовая переменная 4 байта( 1 байт это одна ячейка памяти) 
+                *address2 = 777;
+                Console.WriteLine(*address2); //показало 777
+                //unsafe - небезопастная работа с памятью
+
+                //указетель на указатель
+                int** adr= &address;
+                Console.WriteLine((long)adr);
+                Console.WriteLine((long)*adr);//разименновать
+                Console.WriteLine(**adr);//разименновать полностью
+
+
+                Console.ReadKey();
+            }
+
+        }
+        static unsafe void Calc(int i, int* j) //небезопасный метод
+        {
+            i = 500;//значения не поменяли (значимое )
+            *j = 700; //ссылка поменялась( небезопасный код )
+        }
+
+        static void Calc(int i, ref int j) //безопасный код (эквевалентно вверхнему методу)
+        {
+
+        }
+    }
+}
---
+++ Sanch.Unsafe_Code.Ukazately/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Sanch.Unsafe_Code.Ukazately")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sanch.Unsafe_Code.Ukazately")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("68b3f79e-2cdb-4632-a64d-525da8c40778")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
